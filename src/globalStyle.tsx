ba377701f759dc410dbcfc498161cca68ba6d250
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-09-24 10:10:57 +0900

    Feat: 초기 세팅

---
+++ src/globalStyle.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    //* 폰트
+  }
+`;
+
+export default GlobalStyle;
