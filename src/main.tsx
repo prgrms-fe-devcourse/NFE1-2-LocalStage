099cf046a07fb512e24cba1e93ea737871f9090a
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-09-24 17:13:11 +0900

    Feat: GlobalStyle 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client';
 import 'reset.css';
 import { RouterProvider } from 'react-router-dom';
 import router from '@/router';
+import GlobalStyle from '@/styles/globalStyle';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </StrictMode>,
 );
