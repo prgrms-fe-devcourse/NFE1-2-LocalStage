5c4b6cecf181e1af7c6a6ce10a903ad0706bebb2
thsk3 <bumakker@gmail.com>
2024-09-25 15:10:09 +0900

    Fix: Poster 컴포넌트 반응형으로 수정 - 부모 요소의 크기가 변할 때 이미지의 크기가 비율을 유지하면서 변하도록 수정했습니다. - 반응형으로 만들어주기 위해 Poster가 받는 props는 타입을 number로 수정했습니다. - 준 너비와 높이는 %로 반영됩니다.

--- src/components/Poster/index.tsx
+++ src/components/Poster/index.tsx
@@ -2,10 +2,14 @@ import * as S from './styles';
 
 interface PosterProps {
   src: string;
-  width?: string;
-  height?: string;
+  width?: number;
+  height?: number;
 }
 
-export const Poster = ({ src, width = '235px', height = '330px' }: PosterProps) => {
-  return <S.Poster alt="poster" src={src} width={width} height={height}></S.Poster>;
+export const Poster = ({ src, width = 100, height = 100 }: PosterProps) => {
+  return (
+    <S.Poster width={width} height={height}>
+      <img alt="poster" src={src}></img>
+    </S.Poster>
+  );
 };
--- src/components/Poster/styles.ts
+++ src/components/Poster/styles.ts
@@ -1,7 +1,15 @@
 import { styled } from 'styled-components';
 
-export const Poster = styled.img<{ width: string; height: string }>`
-  width: ${props => props.width};
-  height: ${props => props.height};
-  object-fit: cover;
+export const Poster = styled.div<{ width: number; height: number }>`
+  position: relative;
+  width: ${props => props.width}%;
+  padding-top: ${props => props.height}%;
+  & > img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
