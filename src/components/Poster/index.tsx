06e957b2df1cd2e71ed35c935695c41470a4fe9a
thsk3 <bumakker@gmail.com>
2024-09-25 11:45:30 +0900

    Fix: props 수정 - props 받는 법을 수정했습니다. - styled component generic을 수정했습니다.

--- src/components/Poster/index.tsx
+++ src/components/Poster/index.tsx
@@ -1,12 +1,11 @@
 import * as S from './styles';
 
 interface PosterProps {
-  src?: string;
+  src: string;
   width?: number;
   height?: number;
 }
 
-export const Poster = (props: PosterProps) => {
-  const { src = 'http://placehold.co/235x330?text=poster', width = 235, height = 330 } = props;
+export const Poster = ({ src, width = 235, height = 330 }: PosterProps) => {
   return <S.Poster alt="poster" src={src} width={width} height={height}></S.Poster>;
 };
--- src/components/Poster/styles.ts
+++ src/components/Poster/styles.ts
@@ -1,6 +1,7 @@
 import { styled } from 'styled-components';
 
-export const Poster = styled.img`
-  width: ${props => props.width} + 'px';
-  height: ${props => props.height} + 'px';
+export const Poster = styled.img<{ width: number; height: number }>`
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+  object-fit: cover;
 `;
