fa55dd29c84a4f5849165356b1fceca3d6a7196d
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-09-26 09:47:16 +0900

    Refactor: API Type 주석을 자동완성이 되는 주석 형식으로 변경

--- src/types/apis.ts
+++ src/types/apis.ts
@@ -1,118 +1,427 @@
 // 공통 RequestType
 interface CommonRequestType {
-  service: string; // 서비스키 (필수)
-  stdate?: string; // 공연시작일
-  eddate?: string; // 공연종료일
-  cpage?: string; // 현재페이지 (필수)
-  rows?: string; // 페이지당목록 수 (필수)
-  shprfnm?: string; // 공연명
-  shcate?: string; // 장르코드
-  prfplccd?: string; // 공연장코드
-  signgucode?: string; // 지역(시도)코드
-  signgucodesub?: string; // 지역(구군)코드
-  kidstate?: string; // 아동공연여부
-  prfstate?: string; // 공연상태코드
-  ststype?: string; // 통계기준
-  date?: string; // 기준일
-  catecode?: string; // 장르코드
-  area?: string; // 지역코드
-  srchseatscale?: string; // 좌석수코드
-  shprfnmfct?: string; // 공연 시설 명
-  openrun?: string; // 오픈런 여부 Y/N
+  /**
+   * 서비스키 (필수)
+   */
+  service: string;
+  /**
+   * 공연시작일
+   */
+  stdate?: string;
+  /**
+   * 공연종료일
+   */
+  eddate?: string;
+  /**
+   * 현재페이지 (필수)
+   */
+  cpage?: string;
+  /**
+   * 페이지당목록 수 (필수)
+   */
+  rows?: string;
+  /**
+   * 공연명
+   */
+  shprfnm?: string;
+  /**
+   * 장르코드
+   */
+  shcate?: string;
+  /**
+   * 공연장코드
+   */
+  prfplccd?: string;
+  /**
+   * 지역(시도)코드
+   */
+  signgucode?: string;
+  /**
+   * 지역(구군)코드
+   */
+  signgucodesub?: string;
+  /**
+   * 아동공연여부
+   */
+  kidstate?: string;
+  /**
+   * 공연상태코드
+   */
+  prfstate?: string;
+  /**
+   * 통계기준
+   */
+  ststype?: string;
+  /**
+   * 기준일
+   */
+  date?: string;
+  /**
+   * 장르코드
+   */
+  catecode?: string;
+  /**
+   * 지역코드
+   */
+  area?: string;
+  /**
+   * 좌석수코드
+   */
+  srchseatscale?: string;
+  /**
+   * 공연 시설 명
+   */
+  shprfnmfct?: string;
+  /**
+   * 오픈런 여부 Y/N
+   */
+  openrun?: string;
+  /**
+   * 공연 ID
+   */
   mt20id?: string;
+  /**
+   * 공연 시설 ID
+   */
   mt10id?: string;
 }
 // 공통 ResponseType
 interface Boxof {
-  area: string; // 지역
-  prfdtcnt: string; // 상연횟수
-  prfplcnm: string; // 공연장
-  prfpd: string; // 공연기간
-  cate: string; // 장르
-  prfnm: string; // 공연명
-  rnum: string; // 순위
-  seatcnt: string; // 좌석수
-  poster: string; // 포스터이미지
-  mt20id: string; // 공연ID
+  /**
+   * 지역
+   */
+  area: string;
+  /**
+   * 상연횟수
+   */
+  prfdtcnt: string;
+  /**
+   * 공연장
+   */
+  prfplcnm: string;
+  /**
+   * 공연기간
+   */
+  prfpd: string;
+  /**
+   * 장르
+   */
+  cate: string;
+  /**
+   * 공연명
+   */
+  prfnm: string;
+  /**
+   * 순위
+   */
+  rnum: string;
+  /**
+   * 좌석수
+   */
+  seatcnt: string;
+  /**
+   * 포스터이미지
+   */
+  poster: string;
+  /**
+   * 공연ID
+   */
+  mt20id: string;
 }
 
 interface Db {
-  awards: string; // 수상 실적
-  relateurl: string; // (공연장) 홈페이지
-  telno: string; // 전화번호
-  mt13cnt: string; // 공연장 수
-  seatscale: string; // 객석 수
-  fcltychartr: string; // 시설 특성
-  opende: number; // 개관연도
-  mt20id: string; // 공연ID
-  mt10id: string; // 공연시설ID
-  prfnm: string; // 공연명
-  prfpdfrom: string; // 공연시작일
-  prfpdto: string; // 공연종료일
-  fcltynm: string; // 공연시설명(공연장명)
-  poster: string; // 포스터이미지경로
-  genrenm: string; // 장르명
-  prfstate: string; // 공연상태
-  openrun: string; // 오픈런
-  styurls: { styurl: string }[]; // 소개이미지목록
-  dtguidance: string; // 공연시간
-  prfage: string; // 공연관람연령
-  prfcast: string; // 공연출연진
-  prfcrew: string; // 공연제작진
-  prfruntime: string; // 공연런타임
-  entrpsnm: string; // 제작사
-  entrpsnmP: string; // 제작사
-  entrpsnmA: string; // 기획사
-  entrpsnmH: string; // 주최
-  entrpsnmS: string; // 주관
-  pcseguidance: string; // 티켓가격
-  sty: string; // 줄거리
-  visit: string; // 내한
-  child: string; // 아동
-  daehakro: string; // 대학로
-  festival: string; // 축제
-  musicallicense: string; // 뮤지컬 라이센스
-  musicalcreate: string; // 뮤지컬 창작
-  updatedate: string; // 최종수정일
-  la: number; //위도
-  lo: number; //경도
-  restaurant: string; // 레스토랑
-  cafe: string; //카페
-  store: string; // 편의점
-  nolibang: string; //놀이방
-  suyu: string; //수유실
-  parkbarrier: string; //장애시설
-  restbarrier: string; //장애시설
-  runwbarrier: string; //장애시설
-  elevbarrier: string; //장애시설엘리베이터
-  parkinglot: string; // 주차 시설
+  /**
+   * 수상 실적
+   */
+  awards: string;
+  /**
+   * (공연장) 홈페이지
+   */
+  relateurl: string;
+  /**
+   * 전화번호
+   */
+  telno: string;
+  /**
+   * 공연장 수
+   */
+  mt13cnt: string;
+  /**
+   * 객석 수
+   */
+  seatscale: string;
+  /**
+   * 시설 특성
+   */
+  fcltychartr: string;
+  /**
+   * 개관연도
+   */
+  opende: number;
+  /**
+   * 공연ID
+   */
+  mt20id: string;
+  /**
+   * 공연시설ID
+   */
+  mt10id: string;
+  /**
+   * 공연명
+   */
+  prfnm: string;
+  /**
+   * 공연시작일
+   */
+  prfpdfrom: string;
+  /**
+   * 공연종료일
+   */
+  prfpdto: string;
+  /**
+   * 공연시설명(공연장명)
+   */
+  fcltynm: string;
+  /**
+   * 포스터이미지경로
+   */
+  poster: string;
+  /**
+   * 장르명
+   */
+  genrenm: string;
+  /**
+   * 공연상태
+   */
+  prfstate: string;
+  /**
+   * 오픈런
+   */
+  openrun: string;
+  /**
+   * 소개이미지목록
+   */
+  styurls: { styurl: string }[];
+  /**
+   * 공연시간
+   */
+  dtguidance: string;
+  /**
+   * 공연관람연령
+   */
+  prfage: string;
+  /**
+   * 공연출연진
+   */
+  prfcast: string;
+  /**
+   * 공연제작진
+   */
+  prfcrew: string;
+  /**
+   * 공연런타임
+   */
+  prfruntime: string;
+  /**
+   * 제작사
+   */
+  entrpsnm: string;
+  /**
+   * 제작사
+   */
+  entrpsnmP: string;
+  /**
+   * 기획사
+   */
+  entrpsnmA: string;
+  /**
+   * 주최
+   */
+  entrpsnmH: string;
+  /**
+   * 주관
+   */
+  entrpsnmS: string;
+  /**
+   * 티켓가격
+   */
+  pcseguidance: string;
+  /**
+   * 줄거리
+   */
+  sty: string;
+  /**
+   * 내한
+   */
+  visit: string;
+  /**
+   * 아동
+   */
+  child: string;
+  /**
+   * 대학로
+   */
+  daehakro: string;
+  /**
+   * 축제
+   */
+  festival: string;
+  /**
+   * 뮤지컬 라이센스
+   */
+  musicallicense: string;
+  /**
+   * 뮤지컬 창작
+   */
+  musicalcreate: string;
+  /**
+   * 최종수정일
+   */
+  updatedate: string;
+  /**
+   * 도
+   */
+  la: number;
+  /**
+   * 도
+   */
+  lo: number;
+  /**
+   * 레스토랑
+   */
+  restaurant: string;
+  /**
+   * 페
+   */
+  cafe: string;
+  /**
+   * 편의점
+   */
+  store: string;
+  /**
+   * 이방
+   */
+  nolibang: string;
+  /**
+   * 유실
+   */
+  suyu: string;
+  /**
+   * 애시설
+   */
+  parkbarrier: string;
+  /**
+   * 애시설
+   */
+  restbarrier: string;
+  /**
+   * 애시설
+   */
+  runwbarrier: string;
+  /**
+   * 애시설엘리베이터
+   */
+  elevbarrier: string;
+  /**
+   * 주차 시설
+   */
+  parkinglot: string;
   area: string;
-  pcseguidanc: string; //티켓가격
-  musicallicens: string; // 뮤지컬 라이센스
-  adres: string; // 주소
+  /**
+   * 켓가격
+   */
+  pcseguidanc: string;
+  /**
+   * 뮤지컬 라이센스
+   */
+  musicallicens: string;
+  /**
+   * 주소
+   */
+  adres: string;
 }
 
 interface Prfst {
-  area: string; // 지역
-  prfplccnt: number; // 공연장
-  fcltycnt: number; // 공연시설
-  prfprocnt: number; // 개막편수
-  prfcnt: number; // 공연건수
-  prfdtcnt: number; // 상연횟수
-  seatcnt: number; // 좌석수
-  totnmrs: number; // 총티켓판매수
-  nmrs: number; // 판매수/관객수
-  nmrcancl: number; // 취소수
-  amount: number; // 총티켓판매액
-  cate: string; // 장르
-  amountshr: number; // 매출액점유율
-  nmrsshr: number; // 관객점유율
-  fcltynm: string; // 공연시설명
-  prfnm: string; // 공연명
-  mt20id: string; // 공연ID
-  entrpsnm: string; // 기획/제작사
-  prfpdfrom: string; // 공연시작일
-  prfpdto: string; // 공연종료일
+  /**
+   * 지역
+   */
+  area: string;
+  /**
+   * 공연장
+   */
+  prfplccnt: number;
+  /**
+   * 공연시설
+   */
+  fcltycnt: number;
+  /**
+   * 개막편수
+   */
+  prfprocnt: number;
+  /**
+   * 공연건수
+   */
+  prfcnt: number;
+  /**
+   * 상연횟수
+   */
+  prfdtcnt: number;
+  /**
+   * 좌석수
+   */
+  seatcnt: number;
+  /**
+   * 총티켓판매수
+   */
+  totnmrs: number;
+  /**
+   * 판매수/관객수
+   */
+  nmrs: number;
+  /**
+   * 취소수
+   */
+  nmrcancl: number;
+  /**
+   * 총티켓판매액
+   */
+  amount: number;
+  /**
+   * 장르
+   */
+  cate: string;
+  /**
+   * 매출액점유율
+   */
+  amountshr: number;
+  /**
+   * 관객점유율
+   */
+  nmrsshr: number;
+  /**
+   * 공연시설명
+   */
+  fcltynm: string;
+  /**
+   * 공연명
+   */
+  prfnm: string;
+  /**
+   * 공연ID
+   */
+  mt20id: string;
+  /**
+   * 기획/제작사
+   */
+  entrpsnm: string;
+  /**
+   * 공연시작일
+   */
+  prfpdfrom: string;
+  /**
+   * 공연종료일
+   */
+  prfpdto: string;
 }
 
 //* 1. 공연목록조회서비스
