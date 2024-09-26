2dd8e03eee6b4fbdc495971c6bb1d67ab470466d
thsk3 <bumakker@gmail.com>
2024-09-26 15:10:25 +0900

    Refactor: Poster 랭크 표시 방식 수정

--- src/components/Poster/index.tsx
+++ src/components/Poster/index.tsx
@@ -11,7 +11,7 @@ export const Poster = ({ src, width = 100, height = (4 / 3) * width, rank }: Pos
   return (
     <S.Poster width={width} height={height}>
       <img alt="poster" src={src}></img>
-      <S.Rank>{rank}</S.Rank>
+      {rank && <S.Rank>{rank}</S.Rank>}
     </S.Poster>
   );
 };
