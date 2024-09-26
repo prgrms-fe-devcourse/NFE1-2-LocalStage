77420ec4b6bf8a217db029326a84be4bffd9e746
jymaeng <jymaeng1234@gmail.com>
2024-09-26 17:31:39 +0900

    Fix : key값 수정 및 onChange 추가

--- src/components/StarIconContainer/index.tsx
+++ src/components/StarIconContainer/index.tsx
@@ -1,19 +1,25 @@
 import { useState } from 'react';
 import * as S from './styles';
 import { StarIcon } from '@/components/StarIcon';
+interface StarIconContainerProps {
+  onChange: (count: number) => void;
+}
 
-export const StarIconContainer = () => {
+export const StarIconContainer = ({ onChange }: StarIconContainerProps) => {
   const [checkedStars, setCheckStars] = useState<number | null>(null);
 
   const onClick = (index: number) => {
     setCheckStars(index);
+    if (index !== null) {
+      onChange(index + 1);
+    }
   };
 
   return (
     <S.StarIconContainer>
       {Array.from({ length: 5 }, (_, index) => (
         <StarIcon
-          key={index}
+          key={`star-${index}`}
           isChecked={checkedStars !== null && index <= checkedStars}
           onClick={() => onClick(index)}
         />
