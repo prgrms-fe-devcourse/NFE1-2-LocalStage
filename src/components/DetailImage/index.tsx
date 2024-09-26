bdd2f8e58f94806b99b7ca4d1d6f3a5e91f85d7a
jymaeng <jymaeng1234@gmail.com>
2024-09-26 12:45:57 +0900

    FIx : src만 받아오도록 수정

--- src/components/DetailImage/index.tsx
+++ src/components/DetailImage/index.tsx
@@ -2,10 +2,8 @@ import * as S from './styles';
 
 interface DetailImageProps {
   src: string;
-  width?: number;
-  height?: number;
 }
 
-export const DetailImage = ({ src, width = 1280, height = 1500 }: DetailImageProps) => {
-  return <S.DetailImage alt="공연상세정보" src={src} width={width} height={height} />;
+export const DetailImage = ({ src }: DetailImageProps) => {
+  return <S.DetailImage alt="공연상세정보" src={src} />;
 };
--- src/components/DetailImage/styles.ts
+++ src/components/DetailImage/styles.ts
@@ -1,9 +1,7 @@
 import { styled } from 'styled-components';
 
-export const DetailImage = styled.img<{ width: number; height: number }>`
+export const DetailImage = styled.img`
   width: 100%;
   height: auto;
-  max-width: ${props => props.width}px;
-  max-height: ${props => props.height}px;
   object-fit: cover;
 `;
