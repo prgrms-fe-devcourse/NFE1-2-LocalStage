72b9e877197df6a84dc8ba000841fa3a26d94621
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-09-26 14:23:50 +0900

    Docs: getApiUrl, useFetch 주석 추가

--- src/utils/getApiUrl.ts
+++ src/utils/getApiUrl.ts
@@ -1,3 +1,10 @@
+/**
+ * API URL을 생성하는 함수입니다.
+ *
+ * @param {string} endpoint - API의 엔드포인트를 나타내는 문자열입니다.
+ * @param {T} [requestParams] - 요청에 필요한 매개변수 객체입니다. 선택적으로 제공할 수 있습니다.
+ * @returns {string} - 구성된 API URL을 반환합니다.
+ */
 export default function getApiUrl<T>(endpoint: string, requestParams?: T) {
   const params = new URLSearchParams({
     service: import.meta.env.VITE_API_KEY,
