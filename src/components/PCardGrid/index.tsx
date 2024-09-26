e628938e69a3db60ae866051a175b0762c1c3819
thsk3 <bumakker@gmail.com>
2024-09-26 12:25:01 +0900

    Feat: PCardGrid 컴포넌트 구현

---
+++ src/components/PCardGrid/index.tsx
@@ -0,0 +1,34 @@
+import { PCard } from '@/components/PCard';
+import * as S from './styles';
+
+interface PCardGridProps {
+  performList: {
+    id: string;
+    src: string;
+    title: string;
+    place: string;
+    date: string;
+  }[];
+
+  width?: string;
+  rows?: number;
+  columns?: number;
+  gap?: number;
+}
+
+export const PCardGrid = ({ performList, width = '600px', rows = 1, columns = 5, gap = 10 }: PCardGridProps) => {
+  return (
+    <S.PCardGrid width={width} rows={rows} columns={columns} gap={gap}>
+      {performList.map((perform, index) => (
+        <PCard
+          key={index}
+          src={perform.src}
+          title={perform.title}
+          place={perform.place}
+          date={perform.date}
+          width="100%"
+        ></PCard>
+      ))}
+    </S.PCardGrid>
+  );
+};
