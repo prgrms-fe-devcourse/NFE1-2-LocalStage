b93f9d9d17ef2b3d0ec208ad2fa699c5933d251b
jymaeng <jymaeng1234@gmail.com>
2024-09-26 12:00:28 +0900

    Fix : change grey to gray

--- src/components/StarIcon/styles.ts
+++ src/components/StarIcon/styles.ts
@@ -4,6 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const StarIcon = styled(FontAwesomeIcon)<{ isChecked: boolean }>`
   font-size: 15px;
-  color: ${({ isChecked }) => (isChecked ? theme.colors.yellow : theme.colors.grey)};
+  color: ${({ isChecked }) => (isChecked ? theme.colors.yellow : theme.colors.gray)};
   cursor: pointer;
 `;
