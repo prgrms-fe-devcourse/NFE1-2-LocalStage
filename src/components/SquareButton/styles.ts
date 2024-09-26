82ce714810f9ce24452329d54e813cb1d579d269
MinSeok Kim <kimminsuk051@gmail.com>
2024-09-26 14:11:32 +0900

    Style: gray 컬러 철자 변경

--- src/components/SquareButton/styles.ts
+++ src/components/SquareButton/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const SquareButton = styled.button<{ isActive: boolean }>`
   padding: 15px 36px;
-  border: 1px solid ${({ theme }) => theme.colors.grey};
+  border: 1px solid ${({ theme }) => theme.colors.gray};
   background-color: ${({ theme, isActive }) => (isActive ? theme.colors.primary : theme.colors.white)};
   color: ${({ theme, isActive }) => (isActive ? theme.colors.white : theme.colors.black)};
   cursor: pointer;
