07e1e0b71060e6318f76543ac86297de2ea40142
jymaeng <jymaeng1234@gmail.com>
2024-09-27 09:35:54 +0900

    Fix : key id 수정

--- src/components/DetailImageContainer/index.tsx
+++ src/components/DetailImageContainer/index.tsx
@@ -2,14 +2,14 @@ import { DetailImage } from '@/components/DetailImage';
 import * as S from './styles';
 
 interface DetailImageContainerProps {
-  detailImgList: { src: string }[];
+  detailImgList: { id: string; src: string }[];
 }
 
 export const DetailImageContainer = ({ detailImgList }: DetailImageContainerProps) => {
   return (
     <S.DetailImageContainer>
-      {detailImgList.map((detailImg, index) => (
-        <DetailImage key={index} src={detailImg.src} />
+      {detailImgList.map(detailImg => (
+        <DetailImage key={detailImg.id} src={detailImg.src} />
       ))}
     </S.DetailImageContainer>
   );
