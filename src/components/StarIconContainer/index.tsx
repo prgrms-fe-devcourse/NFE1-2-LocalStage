1f55000dede763a0e83cb7e845a30a4c699872bb
jymaeng <jymaeng1234@gmail.com>
2024-09-27 17:32:03 +0900

    Fix : onChange to updateStarRating

--- src/components/StarIconContainer/index.tsx
+++ src/components/StarIconContainer/index.tsx
@@ -2,17 +2,17 @@ import { useState } from 'react';
 import * as S from './styles';
 import { StarIcon } from '@/components/StarIcon';
 interface StarIconContainerProps {
-  onChange?: (count: number) => void;
+  updateStarRating?: (count: number) => void;
 }
 
 const starsArray = Array.from({ length: 5 });
 
-export const StarIconContainer = ({ onChange }: StarIconContainerProps) => {
+export const StarIconContainer = ({ updateStarRating }: StarIconContainerProps) => {
   const [checkedStars, setCheckedStars] = useState(-1);
 
   const onClick = (index: number) => () => {
     setCheckedStars(index);
-    onChange?.(index + 1);
+    updateStarRating?.(index + 1);
   };
 
   return (
