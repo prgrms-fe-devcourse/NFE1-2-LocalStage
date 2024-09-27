79f1434e5ffa54818d393ef860b61f69e8505880
jymaeng <jymaeng1234@gmail.com>
2024-09-27 09:18:28 +0900

    Fix : key값 및 css 수정

--- src/components/DetailImageContainer/styles.ts
+++ src/components/DetailImageContainer/styles.ts
@@ -1,8 +1,3 @@
 import { styled } from 'styled-components';
 
-export const DetailImageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
+export const DetailImageContainer = styled.div``;
