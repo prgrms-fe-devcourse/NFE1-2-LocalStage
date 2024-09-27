bb899e177c3c691de9c9b3e516fed74d8f008678
jymaeng <jymaeng1234@gmail.com>
2024-09-27 10:22:40 +0900

    Fix: key값 index로 다시 수정

--- src/components/DetailImageContainer/index.tsx
+++ src/components/DetailImageContainer/index.tsx
@@ -2,14 +2,14 @@ import { DetailImage } from '@/components/DetailImage';
 import * as S from './styles';
 
 interface DetailImageContainerProps {
-  detailImgList: { id: string; src: string }[];
+  detailImgList: { src: string }[];
 }
 
 export const DetailImageContainer = ({ detailImgList }: DetailImageContainerProps) => {
   return (
     <S.DetailImageContainer>
-      {detailImgList.map(detailImg => (
-        <DetailImage key={detailImg.id} src={detailImg.src} />
+      {detailImgList.map((detailImg, index) => (
+        <DetailImage key={index} src={detailImg.src} />
       ))}
     </S.DetailImageContainer>
   );
