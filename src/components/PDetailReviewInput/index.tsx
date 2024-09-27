a3176d9b05df63a16f90787c802294543221a30f
chj <bluedog129@gmail.com>
2024-09-27 14:25:47 +0900

    Feat: 컴포넌트 구현

--- src/components/PDetailReviewInput/index.tsx
+++ src/components/PDetailReviewInput/index.tsx
@@ -1,13 +1,41 @@
+import React, { useState } from 'react';
 import * as S from './styles';
 
 interface PDetailReviewInputProps {
   placeholder?: string;
+  value: string;
+  onChange?: (value: string) => void;
 }
 
-export const PDetailReviewInput = ({ placeholder = '공연에 대한 리뷰를 남겨주세요.' }: PDetailReviewInputProps) => {
+export const PDetailReviewInput = ({
+  placeholder = '공연에 대한 리뷰를 남겨주세요.',
+  value,
+  onChange,
+}: PDetailReviewInputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      if (onChange) {
+        onChange(value);
+      }
+    }
+  };
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
   return (
-    <S.PDetailReviewInput>
-      <S.PDetailReviewTextArea placeholder={placeholder} disabled />
-    </S.PDetailReviewInput>
+    <S.PDetailReviewInput
+      placeholder={placeholder}
+      value={value}
+      onChange={e => onChange?.(e.target.value)}
+      onKeyDown={handleKeyDown}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      $isFocused={isFocused}
+      $hasValue={!!value}
+    />
   );
 };
