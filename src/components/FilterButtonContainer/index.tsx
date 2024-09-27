96f54fd257f2d8e6dbe25de558c2e79809884a9c
MinSeok Kim <kimminsuk051@gmail.com>
2024-09-27 18:04:58 +0900

    Refactor: onClick 이중 화살표 함수 패턴으로 변경

--- src/components/FilterButtonContainer/index.tsx
+++ src/components/FilterButtonContainer/index.tsx
@@ -4,16 +4,22 @@ import { FilterButton } from '@/components/FilterButton';
 
 interface FilterButtonContainerProps {
   buttonList: string[];
+  onFilterChange?: (index: number) => void;
 }
 
-export const FilterButtonContainer = ({ buttonList }: FilterButtonContainerProps) => {
+export const FilterButtonContainer = ({ buttonList, onFilterChange }: FilterButtonContainerProps) => {
   const [activeItem, setActiveItem] = useState(0);
+
+  const onClick = (index: number) => () => {
+    setActiveItem(index);
+    onFilterChange?.(index);
+  };
 
   return (
     <S.FilterButtonContainer>
       {buttonList.map((text, index) => (
         <S.FilterButtonWrapper>
-          <FilterButton key={index} text={text} $isActive={index === activeItem} onClick={() => setActiveItem(index)} />
+          <FilterButton key={index} text={text} $isActive={index === activeItem} onClick={onClick(index)} />
           {index < buttonList.length - 1 && <S.StyledSeperator />}
         </S.FilterButtonWrapper>
       ))}
