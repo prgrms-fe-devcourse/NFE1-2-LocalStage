d38cfc05c3b527d36b9512a264848f3fb63382ba
MinSeok Kim <kimminsuk051@gmail.com>
2024-09-27 12:47:21 +0900

    Fix: 기능테스트 코드 삭제

--- src/pages/RankPage.tsx
+++ src/pages/RankPage.tsx
@@ -1,27 +1,8 @@
-import { FilterButton } from '@/components/FilterButton';
-import { useState } from 'react';
-import styled from 'styled-components';
-
-const ButtonGroup = styled.div`
-  display: flex;
-  gap: 10px;
-`;
-
 export default function RankPage() {
-  const [activeFilter, setActiveFilter] = useState<string>('일간');
-
-  const handleFilterClick = (filter: string) => {
-    setActiveFilter(filter);
-    console.log(`${filter} 필터가 선택되었습니다.`);
-  };
-
   return (
     <div>
-      <ButtonGroup>
-        <FilterButton text="일간" $isActive={activeFilter === '일간'} onClick={() => handleFilterClick('일간')} />
-        <FilterButton text="주간" $isActive={activeFilter === '주간'} onClick={() => handleFilterClick('주간')} />
-        <FilterButton text="월간" $isActive={activeFilter === '월간'} onClick={() => handleFilterClick('월간')} />
-      </ButtonGroup>
+      <input placeholder="hello" />
+      <button>asdasd</button>
     </div>
   );
 }
