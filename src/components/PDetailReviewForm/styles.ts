db89f137d4174dba71ea79aafe17aac86597a579
chj <bluedog129@gmail.com>
2024-09-30 12:54:27 +0900

    Feat: 저장하기 버튼을 누르고 focus가 아웃 될 때는 배경색 white 유지 구현(style)

---
+++ src/components/PDetailReviewForm/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.form`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto auto;
+  gap: 15px 35px;
+  width: 100%;
+  max-width: 500px;
+`;
+
+export const Label = styled.label`
+  font-size: 16px;
+  align-self: start;
+  margin-top: 10px;
+`;
+
+export const SubmitButton = styled.button`
+  padding: 10px 20px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.white};
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  justify-self: end;
+  grid-column: 2;
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.gray};
+    cursor: not-allowed;
+  }
+
+  &:hover:not(:disabled) {
+    background-color: ${({ theme }) => theme.colors.gray};
+    color: ${({ theme }) => theme.colors.black};
+  }
+`;
--- src/components/PDetailReviewInput/styles.ts
+++ src/components/PDetailReviewInput/styles.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
-export const PDetailReviewInput = styled.textarea`
+export const PDetailReviewInput = styled.textarea<{ isSubmitted?: boolean }>`
   width: 100%;
   height: 100px;
   padding: 10px;
   border: 1px solid ${({ theme }) => theme.colors.gray};
   border-radius: 10px;
   resize: none;
-  background-color: ${({ theme }) => theme.colors.gray};
+  background-color: ${({ theme, isSubmitted }) => (isSubmitted ? theme.colors.white : theme.colors.gray)};
 
   &::placeholder {
     color: ${({ theme }) => theme.colors.text_gray};
