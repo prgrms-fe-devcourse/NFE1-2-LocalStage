9a20751d90b7cddc752edf4f7731e66ca9794c5f
thsk3 <bumakker@gmail.com>
2024-09-30 11:20:44 +0900

    Feat: PosterWithDesc 컴포넌트 공연장 추가

--- src/components/PosterWithDesc/index.tsx
+++ src/components/PosterWithDesc/index.tsx
@@ -5,16 +5,18 @@ import { H16, P15 } from '@/components/Text';
 interface PosterWithDescProps {
   src: string;
   title: string;
+  place: string;
   date: string;
   width?: string;
 }
 
-export const PosterWithDesc = ({ src, title, date, width = '225px' }: PosterWithDescProps) => {
+export const PosterWithDesc = ({ src, title, place, date, width = '225px' }: PosterWithDescProps) => {
   return (
     <S.PosterWithDesc width={width}>
       <Poster src={src}></Poster>
       <S.Desc>
         <H16>{title}</H16>
+        <P15>{place}</P15>
         <P15>{date}</P15>
       </S.Desc>
     </S.PosterWithDesc>
