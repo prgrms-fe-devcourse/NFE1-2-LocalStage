c01fe262af8e5aea2ad512ce10bc9122df3502e5
chj <bluedog129@gmail.com>
2024-09-30 11:24:14 +0900

    Fix: 타입스크립트로 사용하던 기능을 최대한 css로 해결

--- src/components/PDetailReviewInput/index.tsx
+++ src/components/PDetailReviewInput/index.tsx
@@ -1,32 +1,5 @@
-import React, { useState } from 'react';
 import * as S from './styles';
 
-interface PDetailReviewInputProps {
-  placeholder?: string;
-  value: string;
-  onChange?: (value: string) => void;
-  disabled?: boolean;
-}
-
-export const PDetailReviewInput = ({
-  placeholder = '공연에 대한 리뷰를 남겨주세요.',
-  value,
-  onChange,
-  disabled = false,
-}: PDetailReviewInputProps) => {
-  const [isFocused, setIsFocused] = useState(false);
-
-  return (
-    <S.PDetailReviewInput
-      placeholder={placeholder}
-      value={value}
-      onChange={e => !disabled && onChange?.(e.target.value)}
-      onFocus={() => setIsFocused(true)}
-      onBlur={() => setIsFocused(false)}
-      disabled={disabled}
-      $isFocused={isFocused}
-      $hasValue={!!value}
-      $disabled={disabled}
-    />
-  );
+export const PDetailReviewInput = ({ ...rest }: React.TextareaHTMLAttributes<HTMLTextAreaElement>) => {
+  return <S.PDetailReviewInput {...rest} />;
 };
--- src/components/PDetailReviewInput/styles.ts
+++ src/components/PDetailReviewInput/styles.ts
@@ -1,28 +1,23 @@
 import styled from 'styled-components';
 
-interface StyledPDetailReviewInputProps {
-  $isFocused: boolean;
-  $hasValue: boolean;
-  $disabled: boolean;
-}
-
-export const PDetailReviewInput = styled.textarea<StyledPDetailReviewInputProps>`
+export const PDetailReviewInput = styled.textarea`
   width: 100%;
   height: 100px;
   padding: 10px;
   border: 1px solid ${({ theme }) => theme.colors.gray};
   border-radius: 10px;
   resize: none;
-  background-color: ${({ theme, $isFocused, $hasValue, $disabled }) =>
-    $hasValue || $isFocused || $disabled ? theme.colors.white : theme.colors.bg_gray};
-  cursor: ${({ $disabled }) => ($disabled ? 'default' : 'text')};
+
   color: ${({ theme }) => theme.colors.text_black};
+  background-color: ${({ theme }) => theme.colors.bg_gray};
 
   &::placeholder {
     color: ${({ theme }) => theme.colors.text_gray};
   }
-
   &:focus {
     outline: none;
+  }
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.white};
   }
 `;
