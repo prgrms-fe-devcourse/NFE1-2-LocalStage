96cca43ab6ba58c4afbe9ee1747904b0211034b6
chj <bluedog129@gmail.com>
2024-09-30 12:53:42 +0900

    Feat: 저장하기 버튼을 누르고 focus가 아웃 될 때는 배경색 white 유지 구현

---
+++ src/components/PDetailReviewForm/index.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { PDetailReviewInput } from '../PDetailReviewInput';
+import * as S from './styles';
+
+export const PDetailReviewForm = () => {
+  const [reviewText, setReviewText] = useState('');
+  const [savedReview, setSavedReview] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const onSubmitForm = () => {
+    if (reviewText.trim()) {
+      setSavedReview(reviewText);
+      setIsSubmitted(true);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setReviewText(e.target.value);
+  };
+
+  return (
+    <S.FormContainer>
+      <S.Label>리뷰</S.Label>
+      <PDetailReviewInput
+        value={isSubmitted ? savedReview : reviewText}
+        onChange={onChange}
+        disabled={isSubmitted}
+        isSubmitted={isSubmitted}
+      />
+      <S.SubmitButton onClick={onSubmitForm} disabled={isSubmitted || !reviewText.trim()}>
+        저장하기
+      </S.SubmitButton>
+    </S.FormContainer>
+  );
+};
