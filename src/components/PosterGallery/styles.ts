16767528f1484a174aa4fe66f8ad3d5dd185b667
thsk3 <bumakker@gmail.com>
2024-09-30 11:18:15 +0900

    Feat: PosterGallery 컴포넌트 구현

---
+++ src/components/PosterGallery/styles.ts
@@ -0,0 +1,14 @@
+import { styled } from 'styled-components';
+
+export const PosterGallery = styled.div<{ width: string; gap: number }>`
+  width: ${({ width }) => width};
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: 1fr 1fr repeat(3, 1.2fr);
+  gap: ${({ gap }) => gap}px;
+
+  & > div:first-child {
+    grid-column: 1 / 3;
+    grid-row: 1 / 3;
+  }
+`;
