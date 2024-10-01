f04ec9446142680ffac2c6156ea698fcd1c7955b
MinSeok Kim <kimminsuk051@gmail.com>
2024-10-01 17:16:45 +0900

    Refactor: PItemContainer 코드 리팩토링

--- src/components/PItemContainer/index.tsx
+++ src/components/PItemContainer/index.tsx
@@ -1,47 +1,36 @@
 import { PItem } from '@/components/PItem';
-import usePList from '@/hooks/usePList';
-import { useEffect, useState } from 'react';
+import { CommonResponseType, PListResponseType } from '@/types/apis';
 
-interface PListParams {
-  stdate: string;
-  eddate: string;
-  cpage: number;
-  rows: number;
-  prfstate: string;
-  signgucode: string;
-  sortStdr: string;
+type DbItem = Pick<CommonResponseType, 'mt20id' | 'prfnm' | 'fcltynm' | 'prfpdfrom' | 'prfpdto' | 'poster' | 'area'>;
+
+interface PItemContainerProps {
+  data: PListResponseType | undefined;
+  isLoading: boolean;
 }
 
-export const PItemContainer = () => {
-  const [params, setParams] = useState<PListParams>({
-    stdate: '', // 시작일
-    eddate: '', // 종료일
-    cpage: 1, // 현재 페이지
-    rows: 50, // 불러올 목록
-    prfstate: '02', // 공연중
-    signgucode: '11', // 전국
-    sortStdr: '3', // 관람객 수 기준 내림차순
-  });
+// Pick<CommonResponseType,"area"|"mt20id">
+interface PItemListProps {
+  data: DbItem[];
+}
 
-  useEffect(() => {
-    const today = new Date();
-    const formattedDate = formatDate(today);
-    setParams(prev => ({
-      ...prev,
-      stdate: formattedDate,
-      eddate: formattedDate,
-    }));
-  }, []);
+const PItemList = ({ data }: PItemListProps) => (
+  <>
+    {data.map((item, index) => (
+      <PItem
+        key={item.mt20id}
+        rank={index + 1}
+        id={item.mt20id}
+        title={item.prfnm}
+        location={item.fcltynm}
+        period={`${item.prfpdfrom} ~ ${item.prfpdto}`}
+        posterUrl={item.poster}
+        area={item.area}
+      />
+    ))}
+  </>
+);
 
-  const formatDate = (date: Date): string => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}${month}${day}`;
-  };
-
-  const { data, isLoading } = usePList(params);
-
+export const PItemContainer = ({ data, isLoading }: PItemContainerProps) => {
   if (isLoading) {
     return <div>로딩 중...</div>;
   }
@@ -50,20 +39,5 @@ export const PItemContainer = () => {
     return <div>데이터를 불러올 수 없습니다.</div>;
   }
 
-  return (
-    <>
-      {data.dbs.db.map((item, index) => (
-        <PItem
-          key={item.mt20id}
-          rank={index + 1}
-          id={item.mt20id}
-          title={`${item.prfnm}`}
-          location={item.fcltynm}
-          period={`${item.prfpdfrom} ~ ${item.prfpdto}`}
-          posterUrl={item.poster}
-          area={item.area}
-        />
-      ))}
-    </>
-  );
+  return <PItemList data={data.dbs.db} />;
 };
