268a802247bd549591738d1b1886ee00b06330e5
chj <bluedog129@gmail.com>
2024-10-01 17:45:48 +0900

    Fix: 가상요소 border 수정 및 border-top 추가

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,11 +4,23 @@ export const FooterContainer = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
+  border-top: 1px solid ${({ theme }) => theme.colors.gray};
 
   & > a {
     padding: 0 10px;
+    position: relative;
+
     &:not(:last-child) {
-      border-right: 1px solid ${({ theme }) => theme.colors.gray};
+      &::after {
+        content: '';
+        position: absolute;
+        right: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 1px;
+        height: 20px;
+        background-color: ${({ theme }) => theme.colors.gray};
+      }
     }
   }
 `;
@@ -18,4 +30,5 @@ export const Logo = styled.img`
   width: 110px;
   object-fit: contain;
   position: relative;
+  margin: 10px 5px;
 `;
