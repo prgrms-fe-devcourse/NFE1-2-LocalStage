02d3768d67ea811708763924eebb95f87096e3b2
jymaeng <jymaeng1234@gmail.com>
2024-10-02 17:20:21 +0900

    Feat : PosterWithButtons 컴포넌트 구현

---
+++ src/components/PosterWithButtons/index.tsx
@@ -0,0 +1,34 @@
+import { Poster } from '@/components/Poster';
+import { ShareButton } from '@/components/ShareButton';
+import { HeartButton } from '@/components/HeartButton';
+import * as S from './styles';
+import { useState } from 'react';
+
+interface PosterWithButtonsProps {
+  src: string;
+  url: string;
+  initialIsFilled?: boolean;
+  width?: number;
+}
+
+export const PosterWithButtons = ({ src, url, initialIsFilled = false, width = 20 }: PosterWithButtonsProps) => {
+  const [isFilled, setIsFilled] = useState(initialIsFilled);
+  console.log('Poster:', src);
+
+  const onClickHeart = () => {
+    setIsFilled(prev => !prev);
+  };
+
+  return (
+    <S.PosterWithButtons width={width}>
+      <Poster src={src} width="100%" />
+      <S.PosterButtonContainer>
+        <S.HeartBtnWithText>
+          <HeartButton initialFilled={isFilled} onClickHeartButton={onClickHeart} />
+          <p>찜하기</p>
+        </S.HeartBtnWithText>
+        <ShareButton url={url} />
+      </S.PosterButtonContainer>
+    </S.PosterWithButtons>
+  );
+};
---
+++ src/components/PosterWithButtons/styles.ts
@@ -0,0 +1,22 @@
+import { styled } from 'styled-components';
+
+export const PosterWithButtons = styled.div<{ width: number }>`
+  width: ${({ width }) => width}%;
+`;
+
+export const PosterButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 5px;
+`;
+
+export const HeartBtnWithText = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 2px;
+
+  &:hover {
+    transform: scale(1.1);
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
