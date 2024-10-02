7e9c9a1dba0c104e89b2935beb975fbafdad5865
MinSeok Kim <kimminsuk051@gmail.com>
2024-10-02 09:25:33 +0900

    Refactor: Clicked -> Filled로 수정

--- src/components/HeartButton/styles.ts
+++ src/components/HeartButton/styles.ts
@@ -1,8 +1,8 @@
 import { styled } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export const HeartButton = styled(FontAwesomeIcon)<{ $isClicked: boolean }>`
-  color: ${({ theme, $isClicked }) => ($isClicked ? theme.colors.primary : theme.colors.gray)};
+export const HeartButton = styled(FontAwesomeIcon)<{ $isFilled: boolean }>`
+  color: ${({ theme, $isFilled }) => ($isFilled ? theme.colors.primary : theme.colors.gray)};
   cursor: pointer;
   transition: color 0.1s ease;
   font-size: 18px;
