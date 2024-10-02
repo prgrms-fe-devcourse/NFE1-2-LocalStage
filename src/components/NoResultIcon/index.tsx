e34534b49b7bb9c73b46e75ac7c66aeeddadbe52
MinSeok Kim <kimminsuk051@gmail.com>
2024-10-02 10:27:41 +0900

    Refactor: 컴포넌트 네이밍 수정

--- src/components/NoResultIcon/index.tsx
+++ src/components/NoResultIcon/index.tsx
@@ -2,9 +2,9 @@ import * as S from './styles';
 
 export const NoResultIcon = () => {
   return (
-    <S.NoResultIcon>
-      <S.Icon src="src/assets/imgs/noResultIcon.svg" alt="noResultLogo" />
-      <S.Message>검색 결과가 존재하지 않습니다.</S.Message>
-    </S.NoResultIcon>
+    <S.NoResultContainer>
+      <S.NoResultIcon src="src/assets/imgs/noResultIcon.svg" alt="noResultLogo" />
+      <S.NoResultText>검색 결과가 존재하지 않습니다.</S.NoResultText>
+    </S.NoResultContainer>
   );
 };
--- src/components/NoResultIcon/styles.ts
+++ src/components/NoResultIcon/styles.ts
@@ -1,19 +1,19 @@
 import { H32 } from '@/components/Text';
 import styled from 'styled-components';
 
-export const NoResultIcon = styled.div`
+export const NoResultContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
 `;
 
-export const Icon = styled.img`
+export const NoResultIcon = styled.img`
   width: 200px;
   height: 200px;
 `;
 
-export const Message = styled(H32)`
+export const NoResultText = styled(H32)`
   margin-top: 2rem;
   color: ${({ theme }) => theme.colors.black};
 `;
