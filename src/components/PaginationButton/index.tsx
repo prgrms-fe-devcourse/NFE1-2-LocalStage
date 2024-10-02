7aaa91c1dd2a1e029263be5c015fd84a61120729
chj <bluedog129@gmail.com>
2024-10-02 11:32:19 +0900

    Refactor: 불필요한 주석, import 제거

--- src/components/PaginationButton/index.tsx
+++ src/components/PaginationButton/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import * as S from './styles';
 
 interface PaginationButtonProps {
-  number: number; // 페이지 번호
-  isActive: boolean; // 현재 활성화된 페이지인지 여부
-  onClick: (number: number) => void; // 클릭 이벤트 핸들러
+  number: number;
+  isActive: boolean;
+  onClick: (number: number) => void;
 }
 
 export const PaginationButton = ({ number, isActive, onClick }: PaginationButtonProps) => {
