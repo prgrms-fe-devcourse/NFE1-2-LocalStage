3d5f3db8c1148d71af4dd779cf57c7c3a9ba69c1
MinSeok Kim <kimminsuk051@gmail.com>
2024-10-02 15:36:23 +0900

    Refactor: 코드 구조 수정

--- src/components/LocalDateTime/index.tsx
+++ src/components/LocalDateTime/index.tsx
@@ -2,28 +2,21 @@ import { useEffect, useState } from 'react';
 import * as S from './styles';
 
 export const LocalDateTime = () => {
-  const [currentDateTime, setCurrentDateTime] = useState('');
+  const [renderDateTime, setRenderDateTime] = useState('');
 
   useEffect(() => {
-    const updateDateTime = () => {
-      const now = new Date();
-      const formattedDate = new Intl.DateTimeFormat('ko-KR', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-      }).format(now);
+    const currentTime = new Date();
+    const dateTimeFormat = new Intl.DateTimeFormat('ko-KR', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    }).format(currentTime);
 
-      setCurrentDateTime(formattedDate.replace(/\./g, '') + ' 기준');
-    };
-
-    updateDateTime();
-    const timer = setInterval(updateDateTime, 60000); // 1분마다 업데이트
-
-    return () => clearInterval(timer);
+    setRenderDateTime(dateTimeFormat + ' 기준');
   }, []);
 
-  return <S.LocalDateTime>{currentDateTime}</S.LocalDateTime>;
+  return <S.LocalDateTime>{renderDateTime}</S.LocalDateTime>;
 };
