1d457a33da7d4aa1c3ca4879ef9e6bd5a616a92b
chj <bluedog129@gmail.com>
2024-10-02 09:41:54 +0900

    Fix: 컴포넌트 최적화

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,30 +1,18 @@
+import { Seperator } from '@/components/Seperator';
 import styled from 'styled-components';
-
 export const FooterContainer = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
   border-top: 1px solid ${({ theme }) => theme.colors.gray};
-
   & > a {
     padding: 0 10px;
     position: relative;
-
-    &:not(:last-child) {
-      &::after {
-        content: '';
-        position: absolute;
-        right: 0;
-        top: 50%;
-        transform: translateY(-50%);
-        width: 1px;
-        height: 20px;
-        background-color: ${({ theme }) => theme.colors.gray};
-      }
-    }
   }
 `;
-
+export const StyledSeperator = styled(Seperator)`
+  height: 20px;
+`;
 export const Logo = styled.img`
   height: 35px;
   width: 110px;
