0254c12d1042f7d9ddb8a035b061928c9b63a269
chj <bluedog129@gmail.com>
2024-10-02 09:49:27 +0900

    Feat: StyledFooter컴포넌트로 Footer에 max-width 지정

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,18 +1,26 @@
 import { Seperator } from '@/components/Seperator';
 import styled from 'styled-components';
+
 export const FooterContainer = styled.footer`
+  border-top: 1px solid ${({ theme }) => theme.colors.gray};
+`;
+
+export const StyledFooter = styled.div`
+  max-width: ${props => props.theme.layout.max_width};
+  margin: 0 auto;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-top: 1px solid ${({ theme }) => theme.colors.gray};
   & > a {
     padding: 0 10px;
     position: relative;
   }
 `;
+
 export const StyledSeperator = styled(Seperator)`
   height: 20px;
 `;
+
 export const Logo = styled.img`
   height: 35px;
   width: 110px;
