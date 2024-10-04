ef82b10972124371013bb9554e660b1c95adbb25
chj <bluedog129@gmail.com>
2024-10-04 15:37:40 +0900

    Feat: NextButton 구현

---
+++ src/components/NextButton/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+
+  svg {
+    width: 16px;
+    height: 16px;
+    margin-left: 8px; // 이 부분이 변경되었습니다
+  }
+`;
+
+export const ButtonText = styled.span`
+  font-size: 15px;
+  font-weight: 500;
+`;
