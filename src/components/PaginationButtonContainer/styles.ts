76b0356106e1178a10afed24f05d8d400253796a
chj <bluedog129@gmail.com>
2024-10-04 14:49:53 +0900

    Feat: PaginationButtonContainer 스타일 구현

---
+++ src/components/PaginationButtonContainer/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const PaginationButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+`;
