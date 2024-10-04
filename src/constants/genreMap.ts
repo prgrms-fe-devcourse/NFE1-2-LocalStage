e21602fbaa55307bbadfe450b8bc04ae6a55da20
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-04 14:15:11 +0900

    Feat: genreMap.ts

---
+++ src/constants/genreMap.ts
@@ -0,0 +1,46 @@
+export const genreMap = {
+  /**
+   * 연극
+   */
+  PLAY: 'AAAA',
+
+  /**
+   * 무용(서양/한국무용)
+   */
+  DANCE_WESTERN_KOREAN: 'BBBC',
+
+  /**
+   * 대중무용
+   */
+  DANCE_POPULAR: 'BBBE',
+
+  /**
+   * 서양음악(클래식)
+   */
+  MUSIC_CLASSICAL: 'CCCA',
+
+  /**
+   * 한국음악(국악)
+   */
+  MUSIC_KOREAN_TRADITIONAL: 'CCCC',
+
+  /**
+   * 대중음악
+   */
+  MUSIC_POPULAR: 'CCCD',
+
+  /**
+   * 복합
+   */
+  COMPLEX: 'EEEA',
+
+  /**
+   * 서커스/마술
+   */
+  CIRCUS_MAGIC: 'EEEB',
+
+  /**
+   * 뮤지컬
+   */
+  MUSICAL: 'GGGA',
+} as const;
