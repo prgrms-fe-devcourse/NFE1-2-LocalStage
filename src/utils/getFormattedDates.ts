2e510f849691c950811279fbb36608fffbde7efe
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-04 20:49:59 +0900

    Feat: getFormattedDates yesterday, tomorrow 추가

--- src/utils/getFormattedDates.ts
+++ src/utils/getFormattedDates.ts
@@ -1,5 +1,9 @@
 export default function getFormattedDates() {
   const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
   const oneMonthAgo = new Date(today);
   oneMonthAgo.setMonth(today.getMonth() - 1);
   const oneMonthLater = new Date(today);
@@ -14,6 +18,7 @@ export default function getFormattedDates() {
 
   return {
     oneMonthAgo: formatDate(oneMonthAgo),
+    yesterday: formatDate(yesterday),
     today: formatDate(today),
     oneMonthLater: formatDate(oneMonthLater),
   };
