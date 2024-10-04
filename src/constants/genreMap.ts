0456751ab881f7f50423260ceac3a54856fd1365
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-04 15:07:24 +0900

    Fix: genreMap key값 한국어로

--- src/constants/genreMap.ts
+++ src/constants/genreMap.ts
@@ -1,46 +1,11 @@
 export const genreMap = {
-  /**
-   * 연극
-   */
-  PLAY: 'AAAA',
-
-  /**
-   * 무용(서양/한국무용)
-   */
-  DANCE_WESTERN_KOREAN: 'BBBC',
-
-  /**
-   * 대중무용
-   */
-  DANCE_POPULAR: 'BBBE',
-
-  /**
-   * 서양음악(클래식)
-   */
-  MUSIC_CLASSICAL: 'CCCA',
-
-  /**
-   * 한국음악(국악)
-   */
-  MUSIC_KOREAN_TRADITIONAL: 'CCCC',
-
-  /**
-   * 대중음악
-   */
-  MUSIC_POPULAR: 'CCCD',
-
-  /**
-   * 복합
-   */
-  COMPLEX: 'EEEA',
-
-  /**
-   * 서커스/마술
-   */
-  CIRCUS_MAGIC: 'EEEB',
-
-  /**
-   * 뮤지컬
-   */
-  MUSICAL: 'GGGA',
+  연극: 'AAAA',
+  무용: 'BBBC',
+  대중무용: 'BBBE',
+  서양음악: 'CCCA',
+  한국음악: 'CCCC',
+  대중음악: 'CCCD',
+  복합: 'EEEA',
+  서커스: 'EEEB',
+  뮤지컬: 'GGGA',
 } as const;
