1140407e05f3d777061ee54244a5420574ef8507
thsk3 <bumakker@gmail.com>
2024-10-04 17:33:42 +0900

    Feat: MainPage 장르별 랭킹 panel 구현

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,5 +1,96 @@
+import usePList from '@/hooks/usePList';
+import getFormattedDates from '@/utils/getFormattedDates';
+import useBoxOffice from '@/hooks/useBoxOffice';
+import { genreMap } from '@/constants/genreMap';
+import { useState } from 'react';
+import { Loader } from '@/components/Loader';
+import { SquareButtonContainer } from '@/components/SquareButtonContainer';
+import { PCardSlider } from '@/components/PCardSlider';
+import { PosterGallery } from '@/components/PosterGallery';
+import { Banner } from '@/components/Banner';
+import bannerImage from '@/assets/imgs/banner.png';
+import { PCardGrid } from '@/components/PCardGrid';
 import * as S from './styles';
+import { H32 } from '@/components/Text';
 
 export default function MainPage() {
-  return <S.MainPage>MainPage</S.MainPage>;
+  const genre = genreMap;
+  const formedDate = getFormattedDates();
+  const [selectGenre, setSelectedGenre] = useState<string>(genre.연극);
+  const { data: genreRank, isLoading: genreLoadin } = useBoxOffice({
+    date: formedDate.today,
+    ststype: 'month',
+    area: 11,
+    catecode: selectGenre,
+  });
+  const { data: BoxOffice } = useBoxOffice({ date: formedDate.today, ststype: 'month', area: 11 });
+  const { data: pList } = usePList({
+    cpage: 1,
+    eddate: formedDate.oneMonthLater,
+    rows: 10,
+    stdate: formedDate.today,
+  });
+  const genreButtonList = Object.entries(genre).map(([key, value]) => ({
+    id: value,
+    text: key,
+    onButtonClick: () => {
+      setSelectedGenre(value);
+    },
+  }));
+  return (
+    <S.MainPage>
+      <S.GenreRank width="100%">
+        <H32>장르별 랭킹</H32>
+        <SquareButtonContainer buttonPropsList={genreButtonList} />
+        {genreLoadin ? (
+          <Loader></Loader>
+        ) : (
+          <>
+            <PCardSlider
+              pList={
+                genreRank?.boxofs?.boxof.map(pItem => ({
+                  id: pItem.mt20id,
+                  posterUrl: 'https://www.kopis.or.kr/' + pItem.poster,
+                  name: pItem.prfnm,
+                  facility: pItem.prfplcnm,
+                  period: pItem.prfpd,
+                  rank: pItem.rnum,
+                })) || []
+              }
+            />
+          </>
+        )}
+      </S.GenreRank>
+      <S.PopularPerforms width="100%">
+        <H32>인기 공연</H32>
+        <PosterGallery
+          pList={
+            BoxOffice?.boxofs?.boxof.map(pItem => ({
+              id: pItem.mt20id,
+              posterUrl: 'https://www.kopis.or.kr/' + pItem.poster,
+              name: pItem.prfnm,
+              facility: pItem.prfplcnm,
+              period: pItem.prfpd,
+              rank: pItem.rnum,
+            })) || []
+          }
+        />
+      </S.PopularPerforms>
+      <Banner src={bannerImage}></Banner>
+      <S.CommingSoon width="100%">
+        <PCardGrid
+          pList={
+            pList?.dbs?.db.slice(0, 10).map(pItem => ({
+              id: pItem.mt20id,
+              posterUrl: pItem.poster,
+              name: pItem.prfnm,
+              facility: pItem.fcltynm,
+              period: pItem.prfpdfrom + '-' + pItem.prfpdto,
+            })) || []
+          }
+          columns={5}
+        ></PCardGrid>
+      </S.CommingSoon>
+    </S.MainPage>
+  );
 }
--- src/pages/MainPage/styles.ts
+++ src/pages/MainPage/styles.ts
@@ -1,6 +1,22 @@
+import { Panel } from '@/components/Panel/styles';
+// import { Panel } from '@/components/Panel';
 import { styled } from 'styled-components';
 
 export const MainPage = styled.div`
   margin: 0 auto;
   max-width: ${({ theme }) => theme.layout.max_width};
+
+  & > div {
+    padding: 50px 0;
+  }
 `;
+export const GenreRank = styled(Panel)`
+  text-align: center;
+  & > div:last-child {
+    margin-top: 30px;
+    text-align: start;
+  }
+`;
+
+export const PopularPerforms = styled(Panel)``;
+export const CommingSoon = styled(Panel)``;
