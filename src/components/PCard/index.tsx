cce83c104cbe2feabb68d1001259fb7d55103135
thsk3 <bumakker@gmail.com>
2024-10-05 00:20:44 +0900

    Feat: PCard, Poster 컴포넌트에 navigate 추가 - 클릭하면 디테일 페이지로 이동하도록 함 - props로 공연 id 안주면 navigate 작동 안함

--- src/components/PCard/index.tsx
+++ src/components/PCard/index.tsx
@@ -2,10 +2,16 @@ import * as S from './styles';
 import { H16, P15 } from '@/components/Text';
 import { Poster } from '@/components/Poster';
 import { PItemType } from '@/types/pItem';
+import { useNavigate } from 'react-router-dom';
 
 export const PCard = ({ id, posterUrl, name, facility, period, rank, width = '225px' }: PItemType) => {
+  const navigate = useNavigate();
+  const navigatePDetail = () => () => {
+    if (!id) return;
+    navigate(`/detail/:${id}`);
+  };
   return (
-    <S.PCard width={width}>
+    <S.PCard width={width} onClick={navigatePDetail()}>
       <Poster src={posterUrl} rank={rank} />
       <S.PCardText>
         <H16>{name}</H16>
