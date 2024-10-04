bca1ce81108e221b513e7bb985bffbb78c0fc352
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-04 16:47:27 +0900

    Fix: genreMap 순서 변경

--- src/constants/genreMap.ts
+++ src/constants/genreMap.ts
@@ -1,11 +1,11 @@
 export const genreMap = {
+  뮤지컬: 'GGGA',
   연극: 'AAAA',
   무용: 'BBBC',
   대중무용: 'BBBE',
   서양음악: 'CCCA',
   한국음악: 'CCCC',
   대중음악: 'CCCD',
-  복합: 'EEEA',
   서커스: 'EEEB',
-  뮤지컬: 'GGGA',
+  복합: 'EEEA',
 } as const;
