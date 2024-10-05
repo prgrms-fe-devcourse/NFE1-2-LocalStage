bf41eac4aae54d246cd27d2783d8b93062efce7e
MinSeok Kim <kimminsuk051@gmail.com>
2024-10-05 18:50:58 +0900

    Refactor: 타입분리 및 코드 수정

--- src/components/FilterButtonContainer/index.tsx
+++ src/components/FilterButtonContainer/index.tsx
@@ -1,25 +1,22 @@
 import * as S from './styles';
-import { useState } from 'react';
 import { FilterButton } from '@/components/FilterButton';
 
 interface FilterButtonContainerProps {
   buttonList: string[];
+  activeIndex: number;
   onFilterChange?: (index: number) => void;
 }
 
-export const FilterButtonContainer = ({ buttonList, onFilterChange }: FilterButtonContainerProps) => {
-  const [activeItem, setActiveItem] = useState(0);
-
+export const FilterButtonContainer = ({ buttonList, activeIndex, onFilterChange }: FilterButtonContainerProps) => {
   const onClick = (index: number) => () => {
-    setActiveItem(index);
     onFilterChange?.(index);
   };
 
   return (
     <S.FilterButtonContainer>
       {buttonList.map((text, index) => (
-        <S.FilterButtonWrapper>
-          <FilterButton key={index} text={text} $isActive={index === activeItem} onClick={onClick(index)} />
+        <S.FilterButtonWrapper key={index}>
+          <FilterButton text={text} $isActive={index === activeIndex} onClick={onClick(index)} />
           {index < buttonList.length - 1 && <S.StyledSeperator />}
         </S.FilterButtonWrapper>
       ))}
