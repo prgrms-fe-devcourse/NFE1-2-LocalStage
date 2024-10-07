b8eee02dae04ddc6ed0ad9d9160c3fc3bfc4ea70
thsk3 <bumakker@gmail.com>
2024-10-07 11:37:37 +0900

    Refactor: useYouTube 캐싱 추가

--- src/hooks/useYouTube.ts
+++ src/hooks/useYouTube.ts
@@ -1,41 +1,43 @@
 import { VItemType, YouTubeResponseType } from '@/types/vItem';
 import { useEffect, useState } from 'react';
-
+import { useCacheStore } from '@/store/useCacheStore';
 /**
  *
- * @param keyList 공연 이름 리스트
- * @returns 공연 관련 동영상 리스트
- * @example const videoList = useYouTube({id: PF250529, name: '캣츠', poster: 'https://placeholder.co/300/400'})
+ * @param keyList 공연 정보
+ * @returns 공연 관련 동영상
+ * @example const videoList = useYouTube({id: 'PF250529', name: '캣츠', poster: 'https://via.placeholder.com/200?text=Poster'})
  */
-export default function useYoutube(perform: {
-  id: string | undefined;
-  name: string | undefined;
-  poster: string | undefined;
-}) {
-  const [video, setVideo] = useState<VItemType>();
-  const [id, setId] = useState<string | undefined>();
+export default function useYoutube(perform: { id?: string; name?: string; poster?: string }) {
+  const [vItem, setVItem] = useState<VItemType>();
+  const { caches, registerCache } = useCacheStore(state => state);
+
   useEffect(() => {
-    if (perform.name == undefined || perform.poster == undefined || id === perform.id) return;
-    setId(perform.id);
+    if (perform.id === undefined || perform.name === undefined || perform.poster === undefined) return;
 
     const fetchData = async () => {
       try {
         const videoUrl = `https://www.googleapis.com/youtube/v3/search?q=${perform.name}&maxResults=1&part=snippet&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`;
+        if (caches[videoUrl]) {
+          setVItem(caches[videoUrl].data as VItemType);
+          return;
+        }
         const res = await fetch(videoUrl);
         const data: YouTubeResponseType = await res.json();
-        setVideo({
+        const videoInfo: VItemType = {
           vId: data.items[0].id.videoId,
           vTitle: data.items[0].snippet.title,
           pId: perform.id,
           postSrc: perform.poster || '',
-        });
+        };
+        setVItem(videoInfo);
+        registerCache(videoUrl, videoInfo);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchData();
-  }, [id, perform]);
+  }, [caches, perform, registerCache]);
 
-  return { video };
+  return { vItem };
 }
