5f09dd406865a58aea22a499fd0cf25e6ba8c75a
jymaeng <jymaeng1234@gmail.com>
2024-10-07 10:44:08 +0900

    Fix : 컴포넌트 Width 속성 추가

--- src/components/PosterWithDetailInfo/index.tsx
+++ src/components/PosterWithDetailInfo/index.tsx
@@ -8,15 +8,21 @@ interface PosterWithDetailInfoProps extends PDetailType {
   posterSrc: string;
   initialIsHeartFilled?: boolean;
   posterWidth?: string;
+  width?: string;
   height?: string;
   isNameInclude?: boolean;
   onHeartClick?: (isHeartFilled: boolean) => void;
   onUpdateScore?: (newScore: number) => void;
 }
 
-export const PosterWithDetailInfo = ({ posterWidth = '30%', placeInfo, ...rest }: PosterWithDetailInfoProps) => {
+export const PosterWithDetailInfo = ({
+  width = '80%',
+  posterWidth = '30%',
+  placeInfo,
+  ...rest
+}: PosterWithDetailInfoProps) => {
   return (
-    <S.PosterWithDetailInfo width="80%">
+    <S.PosterWithDetailInfo width={width}>
       <PosterWithButtons width={posterWidth} shareUrl={placeInfo.shareUrl} {...rest} />
       <S.PDetailContainer>
         <PDetailInfo width="100%" placeInfo={placeInfo} {...rest} />
