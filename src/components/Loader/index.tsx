6b683b5e6b0f6468c23006c6daca0fc07fa53c4a
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-07 10:48:56 +0900

    Fix: LoaderWrapper 제거

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,11 +1,6 @@
-import * as S from './styles';
 import theme from '@/styles/theme';
 import { FidgetSpinnerProps, ThreeDots } from 'react-loader-spinner';
 
 export const Loader = ({ ...rest }: FidgetSpinnerProps) => {
-  return (
-    <S.LoaderWrapper>
-      <ThreeDots color={theme.colors.primary} {...rest} />
-    </S.LoaderWrapper>
-  );
+  return <ThreeDots color={theme.colors.primary} {...rest} />;
 };
--- src/components/Loader/styles.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from 'styled-components';
-
-export const LoaderWrapper = styled.div`
-  height: calc(100vh - 63px);
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
