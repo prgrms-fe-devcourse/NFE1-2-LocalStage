6cae7f91d933b60109d73a100e5b6b9d738707cf
thsk3 <bumakker@gmail.com>
2024-10-07 09:45:23 +0900

    Feat: PCardSlider 슬라이더 재렌더링 처리 - 슬라이드를 넘기고 재렌더링하면 넘긴 그대로 남아있는 현상 수정함 - 재렌더링 하면 다시 1번 요소가 보이도록 함

--- src/components/PCardSlider/index.tsx
+++ src/components/PCardSlider/index.tsx
@@ -2,9 +2,9 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import * as S from './styles';
-// import { CommonResponseType } from '@/types/apis';
 import { PCard } from '@/components/PCard';
 import { PItemType } from '@/types/pItem';
+import { useEffect, useRef } from 'react';
 
 interface PCardSliderProps {
   pList: PItemType[];
@@ -13,6 +13,7 @@ interface PCardSliderProps {
 }
 
 export const PCardSlider = ({ pList, width = '100%', gap = '5px' }: PCardSliderProps) => {
+  const sliderRef = useRef<Slider>(null);
   const settings = {
     infinite: false,
     speed: 500,
@@ -20,9 +21,16 @@ export const PCardSlider = ({ pList, width = '100%', gap = '5px' }: PCardSliderP
     slidesToScroll: 1,
     swipeToSlide: true,
   };
+
+  useEffect(() => {
+    if (sliderRef.current) {
+      sliderRef.current.slickGoTo(0);
+    }
+  }, [pList]);
+
   return (
     <S.PCardSlider width={width} gap={gap}>
-      <Slider {...settings}>
+      <Slider ref={sliderRef} {...settings}>
         {pList.map((perform, index) => {
           return <PCard key={index} {...perform} width="100%" />;
         })}
