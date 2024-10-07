5dcbcd1e73bf3f21d6214989df30dcec2347d664
jymaeng <jymaeng1234@gmail.com>
2024-10-07 10:41:59 +0900

    Fix : CSS 수정

--- src/components/PosterWithDetailInfo/styles.ts
+++ src/components/PosterWithDetailInfo/styles.ts
@@ -4,11 +4,9 @@ export const PosterWithDetailInfo = styled.section<{ width: string }>`
   display: flex;
   margin: 30px auto;
   width: ${({ width }) => width};
-  border: 1px solid blue;
 `;
 
 export const PDetailContainer = styled.div`
   flex-grow: 1;
   margin-left: 20px;
-  border: 1px solid red;
 `;
